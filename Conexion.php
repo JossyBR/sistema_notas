43e685ea4b5806119e44f207d2b9f88c55f2b95e
Jossy Benitez <jossybr94@gmail.com>
2023-12-05 22:15:16 -0500

    Se termina de armar estructura proyecto, se realiza el modelo de usuario

---
+++ Conexion.php
@@ -0,0 +1,23 @@
+<?php
+
+ class Conexion {
+
+    protected $db;
+    private $driver = "mysql";
+    private $host = "localhost";
+    private $bd = "notas";
+    private $usuario = "root";
+    private $contrasena = "";
+
+    public function __construct(){
+        try {
+            $db = new PDO ("{$this->driver}:host={$this->host};dbname={this->bd}", $this->usuario, $this->contrasena);
+            $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $db;
+        } catch (PDOException $e) {
+            echo "Ha surgido un error" . $e->getMessage();
+        }
+    }
+ }
+
+?>
---
+++ Usuarios/Modelo/Usuarios.php
@@ -0,0 +1,60 @@
+<?php
+
+require_once('../../Conexion.php');
+session_start(); //esto nos permt hacer uso de la variable $_SESSION
+
+ class Usuarios extends Conexion {
+
+    public function __construct(){
+        $this->db = parent::__construct();
+    }
+
+    //metodo de inicio de sesion
+    public function login($Usuario, $Password){
+        $statement = $this->db->prepare("SELECT * FROM usuarios where USUARIO = :Usuario AND PASSWORD = :Password");
+        $statement->bindParam(':Usuario',$Usuario);
+        $statement->bindParam(':Password',$Password);
+        $statement->execute();
+        //validacion para el registro
+        if($statement->rowCount() == 1){
+            $result = $statement->fetch();
+            $_SESSION['NOMBRE'] = $result["NOMBRE"] . " " . $result['APELLIDO'];
+            $_SESSION['ID'] = $result['ID_USUARIO'];
+            $_SESSION['PERFIL'] = $result['PERFIL'];
+            return true;
+        }
+        return false;
+    }
+
+    public function getNombre(){
+        return $_SESSION['NOMBRE'];
+    }
+
+    public function getId(){
+        return $_SESSION['ID'];
+    }
+
+    public function getPerfil(){
+        return $_SESSION['PERFIL'];
+    }
+
+    //VALIDAR LA SESION
+
+    public function validateSession (){
+        if($_SESSION['ID'] == null){ //en caso de que no EXISTA UNA sesion se redirige al index.ph
+            header('Location: ../../index.php');
+        }
+    }
+
+    public function validateSessionAdministrator(){
+        if($_SESSION['ID'] != null) {
+            if($_SESSION['PERFIL'] === 'Docente') {
+                header('Location: ../../Estudiantes/Pages/index.php');
+            } else {
+                header('Location: ../../index.php');
+            }
+        }
+    }
+ }
+
+?>
